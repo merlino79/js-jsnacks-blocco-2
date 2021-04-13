def1874a782d5edbbf10f6b0e6e5084c00d26871
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-13 21:29:37 +0200

    prova non riuscita

---
+++ JSnack 2/js/script.js
@@ -0,0 +1,52 @@
+//Inserisci un numero, se è pari stampa il numero,
+//se è dispari stampa il numero successivo
+
+
+
+
+//ciclo for
+
+var numeri = parseInt(prompt("inserisci un  numero"));
+
+
+
+
+
+
+var num = numeri;
+if (num % 2) {
+    console.log("numero pari");
+
+
+
+} else {
+    console.log("no");
+}
+
+
+
+
+
+
+
+
+
+
+//ciclo while
+
+// var counter = 0;
+// var contA = 0;
+
+// while (counter < 5) {
+//     var numA = parseInt(prompt("inserisci i numeri"))
+//     console.log(numA);
+//     contA += numA;
+//     counter++;
+// }
+
+// console.log(" il risultato è " +
+//     contA);
+
+
+// var sommaTot2 = document.getElementById("testo-2").innerHTML;
+// document.getElementById("testo-2").innerHTML = "il secondo risultato è " + contA;
